c1b7563a78dace1aea7a1153ef1ecf82cbc9e71e
Eugene Kubesh <codbasters@gmail.com>
2022-03-28 16:09:18 +0300

    update to ng12

--- src/public_api.ts
+++ src/public_api.ts
@@ -3,3 +3,6 @@
  */
 
 export { FsScrollPickerModule } from './app/fs-scroll-picker.module';
+
+export { ScrollPickerComponent } from './app/components/scroll-picker/scroll-picker.component';
+export { ScrollPickerTemplateComponent } from './app/directives/scroll-picker-template.directive';
